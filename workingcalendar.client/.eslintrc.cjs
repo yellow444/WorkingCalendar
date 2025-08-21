e9572448478fcd33f97a0973e7d5ede62ece6134
Maksim Sitnikov <yellow444@gmail.com>
2025-08-21 09:34:30 +0300

    client

--- workingcalendar.client/.eslintrc.cjs
+++ workingcalendar.client/.eslintrc.cjs
@@ -5,9 +5,14 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'prettier'],
-};
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
--- workingcalendar.client/vite.config.ts
+++ workingcalendar.client/vite.config.ts
@@ -1,22 +1,53 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from 'node:url';
 
+import { defineConfig } from 'vite';
+import plugin from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
+import child_process from 'child_process';
+import { env } from 'process';
+
+const baseFolder =
+    env.APPDATA !== undefined && env.APPDATA !== ''
+        ? `${env.APPDATA}/ASP.NET/https`
+        : `${env.HOME}/.aspnet/https`;
+
+const certificateName = "workingcalendar.client";
+const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+
+// if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+//     if (0 !== child_process.spawnSync('dotnet', [
+//         'dev-certs',
+//         'https',
+//         '--export-path',
+//         certFilePath,
+//         '--format',
+//         'Pem',
+//         '--no-password',
+//     ], { stdio: 'inherit', }).status) {
+//         throw new Error("Could not create certificate.");
+//     }
+// }
+
+const target = env.ASPNETCORE_HTTPS_PORT ? `http://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:8080';
+console.log(target);
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: "/", // если будет подпрефикс (Ingress), сменишь на "/app/" и добавишь UsePathBase("/app")
-    build: {
-        outDir: "../WorkingCalendar.Server/wwwroot",
-        emptyOutDir: true,
-        assetsDir: "assets",
-        sourcemap: true,
+    plugins: [plugin()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     },
     server: {
-        port: 5173,
-        strictPort: true,
         proxy: {
-            "/CheckDayWorkingCalendar": "http://localhost:8080",
-            "/GetYearWorkingCalendar": "http://localhost:8080",
-            "/hc": "http://localhost:8080"
-        }
+            '^/WorkingCalendar': {
+                target,
+                secure: false
+            }
+        },
+        port: 3000
     }
-});
+})
